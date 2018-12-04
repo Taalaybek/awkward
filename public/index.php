b04f3903db84f4e5ec1f1516f00bb006ab919f3b
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-12-05 04:03:32 +0600

    Add Request::class instead global GET var

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
-$name = $_GET['name'] ?: 'Alex';
+use Framework\Http\Request;
+
+chdir(dirname(__DIR__));
+require_once "src/Framework/http/Request.php";
+
+### Initialization
+
+$request = new Request();
+
+### Action
+
+$name = $request->getQueryParams()['name'] ?? 'Guest';
 
 header('X-Developer: Alex');
 echo "Hello, " . $name;
