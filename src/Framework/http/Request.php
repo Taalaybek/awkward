3691f11d86991b710991f4784be813c730345f5e
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-12-05 22:27:02 +0600

    Add properties instead global vars

--- src/Framework/http/Request.php
+++ src/Framework/http/Request.php
@@ -3,12 +3,19 @@ namespace Framework\Http;
 
 class Request
 {
-  function getQueryParams(): array
-  {
-      return $_GET;
-  }
-  function getParsedBody()
-  {
-      return $_POST ?: null;
-  }
+    private $queryParams;
+    private $parsedBody;
+    public function __construct(array $queryParams = [], $parsedBody = null)
+    {
+        $this->queryParams = $queryParams;
+        $this->parsedBody = $parsedBody;
+    }
+    public function getQueryParams(): array
+    {
+        return $this->queryParams;
+    }
+    public function getParsedBody()
+    {
+        return $this->parsedBody;
+    }
 }
