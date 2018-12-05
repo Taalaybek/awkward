bbe074c717631a35866d4cd285dc0f532367870b
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-12-05 20:11:00 +0600

    Created test for Request::getQueryParams()

--- tests/Framework/http/RequestTest.php
+++ tests/Framework/http/RequestTest.php
@@ -10,10 +10,19 @@ class RequestTest extends TestCase
     {
         $_GET = [];
         $_POST = [];
-
         $request = new Request();
         $this->assertEquals([], $request->getQueryParams());
         $this->assertNull($request->getParsedBody());
+    }
+
+    public function testGetQueryParams()
+    {
+        $_GET = $data = [
+            'name' => 'Alex'
+        ];
+        $_POST = [];
+        $request = new Request();
+        $this->assertEquals($data, $request->getQueryParams());
     }
 }
 
