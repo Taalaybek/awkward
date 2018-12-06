62b44be59620f03041655706d07c0e1308ef3e52
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-12-07 02:13:01 +0600

    Implemented RouterCollection::get() and RouterCollection::getRoutes() methods

--- src/Framework/Http/Router/RouterCollection.php
+++ src/Framework/Http/Router/RouterCollection.php
@@ -3,5 +3,24 @@ namespace Framework\Http\Router;
 
 class RouterCollection
 {
+  private $routes = [];
   
+  public function get($name, $pattern, $handler, array $tokens = [])
+  {
+    $this->routes[] = [
+      'name' => $name,
+      'pattern' => $pattern,
+      'handler' => $handler,
+      'methods' => ['GET'],
+      'tokens' => $tokens,
+    ];
+  }
+  
+  /**
+   * @return array
+   */
+  public function getRoutes(): array
+  {
+    return $this->routes;
+  }
 }
