fc57bae5299e0c7de0b954e18669b0e0c6420d0b
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-12-08 02:20:46 +0600

    Use addRoute instead property

--- src/Framework/Http/Router/RouteCollection.php
+++ src/Framework/Http/Router/RouteCollection.php
@@ -17,22 +17,22 @@ class RouteCollection
   
   public function any($name, $pattern, $handler, array $tokens = [])
   {
-    $this->routes[] = new Route($name, $pattern, $handler, [], $tokens);
+    $this->addRoute(new Route($name, $pattern, $handler, [], $tokens));
   }
   
   public function get($name, $pattern, $handler, array $tokens = [])
   {
-    $this->routes[] = new Route($name, $pattern, $handler, ['GET'], $tokens);
+    $this->addRoute(new Route($name, $pattern, $handler, ['GET'], $tokens));
   }
   
   public function post($name, $pattern, $handler, array $tokens = [])
   {
-    $this->routes[] = new Route($name, $pattern, $handler, ['POST'], $tokens);
+    $this->addRoute(new Route($name, $pattern, $handler, ['POST'], $tokens));
   }
   
   public function delete($name, $pattern, $handler, array $tokens = [])
   {
-    $this->routes[] = new Route($name, $pattern, $handler, ['DELETE'], $tokens);
+    $this->addRoute(new Route($name, $pattern, $handler, ['DELETE'], $tokens));
   }
   
   public function getRoutes(): array
