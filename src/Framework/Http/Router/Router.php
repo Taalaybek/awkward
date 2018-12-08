3a48c450a4c1e4bbb6594cfe2d4f3defa495bfb3
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-12-09 03:01:14 +0600

    Added docblock section

--- src/Framework/Http/Router/Router.php
+++ src/Framework/Http/Router/Router.php
@@ -2,9 +2,25 @@
 namespace Framework\Http\Router;
 
 use Psr\Http\Message\ServerRequestInterface;
+use Framework\Http\Router\Exception\RequestNotMatchedException;
+use Framework\Http\Router\Exception\RouteNotFoundException;
 
 interface Router
 {
+  
+  /**
+   * @param ServerRequestInterface $request
+   * @throws RequestNotMatchedException
+   * @return Result|null
+   */
   public function match(ServerRequestInterface $request): ?Result;
+  
+  
+  /**
+   * @param $name
+   * @param array $params
+   * @throws RouteNotFoundException;
+   * @return null|string
+   */
   public function generate($name, array $params = []): ?string;
 }
